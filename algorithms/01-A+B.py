289b70b3eb310d354acb222c6b3e6035a5bee366
Ruslan <shakertov@yandex.ru>
2023-09-19 22:56:49 +0500

    Две задачи по алгоритмам

---
+++ algorithms/01-A+B.py
@@ -0,0 +1,19 @@
+# Считайте и сложите два числа.
+# Результат необходимо вывести на стандартный поток вывода
+# или в файл, указанный в условии задачи.
+
+from typing import Tuple
+
+
+def read_input() -> Tuple[int, int]:
+	a = int(input())
+	b = int(input())
+	return a, b
+
+
+def get_sum(a: int, b: int) -> int:
+	summ = a + b
+	return summ
+
+a, b = read_input()
+print(get_sum(a, b))
---
+++ algorithms/02-zipper.py
@@ -0,0 +1,28 @@
+# Даны два массива чисел длины n.
+# Составьте из них один массив длины 2n,
+# в котором числа из входных массивов чередуются
+# (первый — второй — первый — второй — ...).
+# При этом относительный порядок следования чисел из
+# одного массива должен быть сохранён.
+
+from typing import List, Tuple
+
+
+def read_input() -> (Tuple[List[int], List[int]], int):
+	num = int(input())
+	list_1 = list(map(int, list(input().strip().split())))
+	list_2 = list(map(int, list(input().strip().split())))
+
+	return list_1, list_2, num
+
+
+def zipper(list_1: List[int], list_2: List[int]):
+	zip_list = []
+	for i in range(0, num):
+		zip_list.append(list_1[i])
+		zip_list.append(list_2[i])
+	return zip_list
+
+
+list_1, list_2, num = read_input()
+print(*zipper(list_1, list_2))
